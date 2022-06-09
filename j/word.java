f4757b0f892519bef04095d39e53d9abbaf33f0b
shchoi <shchoi9193@naver.com>
2022-06-09 12:19:41 +0900

    코드 수정

--- j/word.java
+++ j/word.java
@@ -1,28 +1,29 @@
 
 public class Word {
 
-  public static void main(String[] args) {
-    Word word = new Word("Hello World");
-    System.out.println(word.isVowel(0));
-    System.out.println(word.isVowel(1));
-  }
+	public static void main(String[] args) {
+		Word word = new Word("Hello World");
+		System.out.println(word.isVowel(0));
+		System.out.println(word.isVowel(1));
+	}
 
-  private String letters;
+	private String letters;
 
-  public Word(String letters) {
-    this.letters = letters.toLowerCase();
-  }
+	public Word(String letters) {
+		this.letters = letters.toLowerCase();
+	}
 
-  // i번째 글자가 모음인지 찾아내기 //a, e ,i ,o ,u
-  public boolean isVowel(int i) {
-    boolean check = false;
+	// i번째 글자가 모음인지 //a, e ,i ,o ,u
+	public boolean isVowel(int i) {
+		boolean check = false;
+		String checkString = letters.substring(i, i + 1);
 
-    if (letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("e")
-        || letters.substring(i, i + 1).equals("i") || letters.substring(i, i + 1).equals("o")
-        || letters.substring(i, i + 1).equals("u")) {
-      check = true;
-    }
-    return check;
-  }
+		if (checkString.equals("a") || checkString.equals("e")
+				|| checkString.equals("i") || checkString.equals("o")
+				|| checkString.equals("u")) {
+			check = true;
+		}
+		return check;
+	}
 
 }
