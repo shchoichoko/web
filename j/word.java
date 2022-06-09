f0f97724a1accc49a39b5386d70ca6edbce3d289
shchoi <shchoi9193@naver.com>
2022-06-09 11:15:00 +0900

    줄 맞추기

--- j/word.java
+++ j/word.java
@@ -1,27 +1,28 @@
 
 public class Word {
 
-	public static void main(String[] args) {
-		Word word = new Word("Hello World");
-		System.out.println(word.isVowel(0));
-		System.out.println(word.isVowel(1));
-	}
-	
-	private String letters;
+  public static void main(String[] args) {
+    Word word = new Word("Hello World");
+    System.out.println(word.isVowel(0));
+    System.out.println(word.isVowel(1));
+  }
 
-	public Word(String letters) {
-		this.letters = letters.toLowerCase();
-	}
-	
-	// i번째 글자가 모음인지 찾아내기		//a, e ,i ,o ,u 
-	public boolean isVowel(int i) {
-		boolean check = false;
-		
-		if(letters.substring(i, i + 1).equals("a")||letters.substring(i, i + 1).equals("e")||letters.substring(i, i + 1).equals("i")||letters.substring(i, i + 1).equals("o")||letters.substring(i, i + 1).equals("u")) {
-			check = true;
-		}
-		return check;
-	}
-	
-	
+  private String letters;
+
+  public Word(String letters) {
+    this.letters = letters.toLowerCase();
+  }
+
+  // i번째 글자가 모음인지 찾아내기 //a, e ,i ,o ,u
+  public boolean isVowel(int i) {
+    boolean check = false;
+
+    if (letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("e")
+        || letters.substring(i, i + 1).equals("i") || letters.substring(i, i + 1).equals("o")
+        || letters.substring(i, i + 1).equals("u")) {
+      check = true;
+    }
+    return check;
+  }
+
 }
